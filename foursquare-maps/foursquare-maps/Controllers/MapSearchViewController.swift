8b2cf595810e8a4b4d5fd3732ac5820bb72ce32f
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-11-05 15:54:39 -0500

    adds private and computed properties to MapSearchVC

--- foursquare-maps/foursquare-maps/Controllers/MapSearchViewController.swift
+++ foursquare-maps/foursquare-maps/Controllers/MapSearchViewController.swift
@@ -11,19 +11,55 @@ import MapKit
 import CoreLocation
 
 class MapSearchViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
     @IBOutlet weak var venueSearchBar: UISearchBar!
     @IBOutlet weak var locationSearchBar: UISearchBar!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var venueImageCollectionView: UICollectionView!
     
+    // MARK: - Private Properties
+    private let locationManager = CLLocationManager()
+    private let initialLocation = CLLocation(latitude: 40.742054, longitude: -73.769417)
+    private let searchRadius: CLLocationDistance = 2000
+    
+    // MARK: - Computed Properties
+    private var venues = [Venue]() {
+        didSet {
+            addMapAnnotations(venues: venues)
+        }
+    }
+    
+    var searchString: String? = nil {
+        didSet {
+            addMapAnnotations(venues: venues)
+        }
+    }
+    
+    // MARK: - Lifecycle functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func listButtonPressed(_ sender: UIButton) {
     }
+    
+    
+    private func addMapAnnotations(venues: [Venue]) {
+        for venue in venues {
+            let annotation: MKPointAnnotation = {
+                let annotation = MKPointAnnotation()
+                annotation.title = venue.name
+                annotation.coordinate = CLLocationCoordinate2D(latitude: venue.location.lat, longitude: venue.location.lng)
+                return annotation
+            }()
+            
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
     
 }
 
