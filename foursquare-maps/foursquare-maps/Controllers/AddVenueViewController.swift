5ac62acf6c2f735ac98c907238b1eb4c79282711
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-11-08 16:31:42 -0500

    Adds extenstions for collectionview datasource and delegates in addvenueVC

--- foursquare-maps/foursquare-maps/Controllers/AddVenueViewController.swift
+++ foursquare-maps/foursquare-maps/Controllers/AddVenueViewController.swift
@@ -27,16 +27,19 @@ class AddVenueViewController: UIViewController {
     @IBAction func addToCollectionButtonPressed(_ sender: UIBarButtonItem) {
     
     }
-    
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension AddVenueViewController: UICollectionViewDelegateFlowLayout {}
+
+extension AddVenueViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    
 }
